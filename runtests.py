5c6ebf926c6e5bf643060ba1ae4d85fcbc374b17
Eric Davis <ed@npri.org>
2011-07-05 10:02:02 -0700

    Can run test suite with runtests.py now

---
+++ runtests.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'override_settings.settings'
+
+def main():
+    from django.test.simple import DjangoTestSuiteRunner
+    runner = DjangoTestSuiteRunner()
+    runner.run_tests(None)
+
+if __name__ == "__main__":
+    main()
