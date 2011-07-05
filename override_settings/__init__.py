c079a621711ea608d7421561dd682f258d13ced4
Eric Davis <ed@npri.org>
2011-07-05 08:44:49 -0700

    Update override_settings to better handle decorated classes

--- override_settings/__init__.py
+++ override_settings/__init__.py
@@ -2,8 +2,11 @@ from __future__ import with_statement
 from django.conf import settings, UserSettingsHolder
 from django.utils.functional import wraps
 
+# Backported from Django trunk (r16377)
 class override_settings(object):
     """
+    Temporarily override Django settings.
+
     Acts as either a decorator, or a context manager.  If it's a decorator it
     takes a function and returns a wrapped function.  If it's a contextmanager
     it's used with the ``with`` statement.  In either event entering/exiting
@@ -19,11 +22,21 @@ class override_settings(object):
     def __exit__(self, exc_type, exc_value, traceback):
         self.disable()
 
-    def __call__(self, func):
-        @wraps(func)
-        def inner(*args, **kwargs):
-            with self:
-                return func(*args, **kwargs)
+    def __call__(self, test_func):
+        from django.test import TestCase
+        if isinstance(test_func, type) and issubclass(test_func, TestCase):
+            class inner(test_func):
+                def _pre_setup(innerself):
+                    self.enable()
+                    super(inner, innerself)._pre_setup()
+                def _post_teardown(innerself):
+                    super(inner, innerself)._post_teardown()
+                    self.disable()
+        else:
+            @wraps(test_func)
+            def inner(*args, **kwargs):
+                with self:
+                    return test_func(*args, **kwargs)
         return inner
 
     def enable(self):
