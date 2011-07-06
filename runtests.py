63e19d17dbe01c1039fbffc3288dfb4a3be98494
Eric Davis <ed@npri.org>
2011-07-06 08:53:43 -0700

    When running tests, make it verbose and only test the override_settings package

--- runtests.py
+++ runtests.py
@@ -5,8 +5,8 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'override_settings.test_settings'
 
 def main():
     from django.test.simple import DjangoTestSuiteRunner
-    runner = DjangoTestSuiteRunner()
-    runner.run_tests(None)
+    runner = DjangoTestSuiteRunner(verbosity=2)
+    runner.run_tests(['override_settings'])
 
 if __name__ == "__main__":
     main()
