683e8b778be0338a02ba3ea9ebca229143b2225e
Eric Davis <ed@npri.org>
2012-06-25 21:48:21 -0700

    Clean up testing infrastructure

--- setup.py
+++ setup.py
@@ -5,24 +5,24 @@ from distutils.core import setup, Command
 
 cmdclass = {}
 
-class TestCommand(Command):
-    description = "run package tests"
-    user_options = [
-        # Use None because 'v' would conflict with global verbose option
-        ("verbosity=", None, "Django test runner verbosity level. [default=1]"),
-    ]
+# class TestCommand(Command):
+#     description = "run package tests"
+#     user_options = [
+#         # Use None because 'v' would conflict with global verbose option
+#         ("verbosity=", None, "Django test runner verbosity level. [default=1]"),
+#     ]
 
-    def initialize_options(self):
-        self.verbosity = 1
+#     def initialize_options(self):
+#         self.verbosity = 1
 
-    def finalize_options(self):
-        self.verbosity = int(self.verbosity)
+#     def finalize_options(self):
+#         self.verbosity = int(self.verbosity)
 
-    def run(self):
-        from override_settings.run_tests import run_tests
-        sys.exit(run_tests(self.verbosity))
+#     def run(self):
+#         from override_settings.run_tests import run_tests
+#         sys.exit(run_tests(self.verbosity))
 
-cmdclass['test'] = TestCommand
+# cmdclass['test'] = TestCommand
 
 setup(
     name             = "django-override-settings",
